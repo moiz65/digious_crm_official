b56d11e5fb3a699f2c7da58b183b6e75029664fe
Muhammadh4mza <muhammadhamza3323@gmail.com>
2026-01-24 00:59:12 +0500

    updated

--- Frontend/src/components/EmployeeAttendancePage.jsx
+++ Frontend/src/components/EmployeeAttendancePage.jsx
@@ -1276,7 +1276,7 @@ const AttendanceSheet = ({ attendanceData, onExport, onFilter }) => {
           </div>
 
           {/* Search */}
-          <div className="relative">
+          {/* <div className="relative">
             <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-gray-400" />
             <input
               type="text"
@@ -1285,7 +1285,7 @@ const AttendanceSheet = ({ attendanceData, onExport, onFilter }) => {
               onChange={(e) => setSearchTerm(e.target.value)}
               className="pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 text-sm"
             />
-          </div>
+          </div> */}
 
           {/* Filter */}
           <select
